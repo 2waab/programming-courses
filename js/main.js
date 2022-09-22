5ec059fdf4157f86fa94ffb25690c05d6870ab6e
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2022-09-22 03:41:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Show & Hide Tabs
+const tabs = document.querySelectorAll('[data-target]'), tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+    tab.onclick = () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach(tc => {
+            tc.classList.remove('boxes-active');
+        });
+        target.classList.add('boxes-active');
+        tabs.forEach(t => {
+            t.classList.remove('active');
+        });
+        tab.classList.add('active');
+    };
+});
 // Full Screen Navigathion
 let toggler = document.querySelector(".toggle");
 let nav = document.querySelector("header ul");
